2b8e3a609b618d71ab051c51451fd6bb27953265
Ulisses Mendonca Junior <mendocs@gmail.com>
2021-04-12 15:47:25 -0300

    teste pub5

--- Massoterapia.API/Controllers/WeatherForecastController.cs
+++ Massoterapia.API/Controllers/WeatherForecastController.cs
@@ -29,7 +29,7 @@ namespace Massoterapia.API.Controllers
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = new DateTime(2000,1,1),
+                Date = new DateTime(2001,1,1),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
